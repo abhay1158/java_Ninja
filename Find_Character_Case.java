706a113c986ad36b25d173fd7db7a6bc85feac6b
abhay1158 <67824650+abhay1158@users.noreply.github.com>
2022-07-15 08:04:00 +0530

    Add files via upload
    Find Character Case
    
    Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
    1, if the character is an uppercase alphabet (A - Z)
    0, if the character is a lowercase alphabet (a - z)
    -1, if the character is not an alphabet
    
    Input format :
    Single Character
    Output format :
    1 or 0 or -1
    
    Constraints :
    Input can be any character
    
    Sample Input 1 :
    v
    Sample Output 1 :
    0
    
    Sample Input 2 :
    V
    Sample Output 2 :
    1
    
    Sample Input 3 :
    #
    Sample Output 3 :
    -1

---
+++ Find_Character_Case.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Solution {
+    
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner scan = new Scanner(System.in);
+		String str = scan.nextLine();
+		char ch = str.charAt(0);
+		if(ch >= 'A' && ch <= 'Z') {
+			System.out.println(1);
+		}else if(ch >= 'a' && ch <='z') {
+			System.out.println(0);
+		}else {
+			System.out.println(-1);
+		}
+
+    }
+}
