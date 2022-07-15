5a52b019101fe44d3c303a699e57b3ea5b581a1e
abhay1158 <67824650+abhay1158@users.noreply.github.com>
2022-07-15 08:41:04 +0530

    Add files via upload
    Find power of a number
    
    Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to print the answer.
    Note : For this question, you can assume that 0 raised to the power of 0 is 1
    
    
    Input format :
    Two integers x and n (separated by space)
    Output Format :
    x^n (i.e. x raise to the power n)
    
    Constraints:
    0 <= x <= 8 
    0 <= n <= 9
    
    Sample Input 1 :
     3 4
    Sample Output 1 :
    81
    
    Sample Input 2 :
     2 5
    Sample Output 2 :
    32

---
+++ Find_power_of_a_number.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		int n = s.nextInt();
+        System.out.println((int)Math.pow(x, n));
+    }
+}
