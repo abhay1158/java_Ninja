4aed0243e3e495097ecc7035f79b3f1ea35fcdaa
abhay1158 <67824650+abhay1158@users.noreply.github.com>
2022-07-15 09:01:34 +0530

    Add files via upload
    Code : Mirror Number Pattern
    
    Print the following pattern for the given N number of rows.
    Pattern for N = 4
    sss1
    ss12
    s123
    1234
    
    The s represent spaces.
    
    Input format :
    Integer N (Total no. of rows)
    Output format :
    Pattern in N lines
    
    Constraints
    0 <= N <= 50
    
    Sample Input 1:
    3
    Sample Output 1:
    ss1 
    s12
    123
    
    Sample Input 2:
    4
    Sample Output 2:
          1 
        12
      123
    1234

---
+++ Mirror_Number_Pattern.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int i = 1;
+        while(i<=N){
+            int j = 1;
+            while(j<=(N-i)){
+                System.out.print(" ");
+                j++;
+
+            }
+            j = 1;
+            while(j<=i){
+                System.out.print(j);
+                j++;
+
+            }
+            i++;
+            System.out.println();
+        }
+		
+	}
+
+}
